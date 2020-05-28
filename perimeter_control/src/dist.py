41b083f7e51077d8c8bd1df4aeadb6a55cb09c51
Usama-Arshad16 <um851249@gmail.com>
2020-05-28 21:05:45 +0500

    first commit

---
+++ perimeter_control/src/dist.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+import rospy, cv2, cv_bridge
+import cv2.aruco as aruco
+import sys, time, math
+from sensor_msgs.msg import Image
+import numpy as np
+
+
+marker_size  = 50 #- [cm]
+
+
+
+def main(msg):
+
+    #--- Get the camera calibration path
+    calib_path  = ""
+    camera_matrix   = np.loadtxt("/home/jhala/catkin_ws/src/perimeter_robot/perimeter_control/src/cameraMatrix.txt", delimiter=',')
+    camera_distortion   = np.loadtxt('/home/jhala/catkin_ws/src/perimeter_robot/perimeter_control/src/cameraDistortion.txt', delimiter=',')
+
+
+
+    #--- 180 deg rotation matrix around the x axis
+    R_flip  = np.zeros((3,3), dtype=np.float32)
+    R_flip[0,0] = 1.0
+    R_flip[1,1] =-1.0
+    R_flip[2,2] =-1.0
+
+    #--- Define the aruco dictionary
+    aruco_dict  = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)
+    parameters  = aruco.DetectorParameters_create()
+
+
+    #--- Capture the videocamera (this may also be a video or a picture)
+    cap = cv_bridge.CvBridge()
+
+
+
+    #-- Font for the text in the image
+    font = cv2.FONT_HERSHEY_PLAIN
+
+    #while True:
+
+    #-- Read the camera frame
+    frame = cap.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+    #cap = cv2.VideoCapture(frame)
+    #ret, frame = cap.read(frame)
+
+    #-- Convert in gray scale
+    gray    = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # remember, OpenCV stores color images in Blue, Green, Red
+
+    #-- Find all the aruco markers in the image
+    corners, ids, rejected = aruco.detectMarkers(image=gray, dictionary=aruco_dict, parameters=parameters,
+                              cameraMatrix=camera_matrix, distCoeff=camera_distortion)
+
+    
+    if ids is not None:
+	print "Row = " + str(ids[0])
+
+    cv2.imshow('bottom_camera', frame)
+
+    #--- use 'q' to quit
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord('q'):
+        cv2.destroyAllWindows()
+
+def land():
+	global cmd_vel_pub, pub, pub
+	rospy.init_node('landing')
+	image_sub = rospy.Subscriber('/front_cam/camera/image', Image, main)
+	
+
+	
+
+if __name__ == '__main__':
+		cv2.destroyAllWindows()
+		land()
+		rospy.spin()
